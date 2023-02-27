352ca0121510741aea8a22e269b6bedc9dc21b6f
farhan <farhanj.mohamed@gmail.com>
2023-02-27 14:05:38 -0800

    intial commit

---
+++ Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  private float _jumpEndTime;
+  [SerializeField] private float _jumpVelocity = 5;
+  [SerializeField] private float _jumpDuration = 0.5f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    var hortizontal = Input.GetAxis("Horizontal");
+    Debug.Log(hortizontal);
+    Rigidbody2D rb = GetComponent<Rigidbody2D>();
+    var vertical = rb.velocity.y;
+
+    if (Input.GetButtonDown("Jump"))
+      _jumpEndTime = Time.time + _jumpDuration;
+
+    if (Input.GetButton("Jump") && _jumpEndTime > Time.time)
+      vertical = _jumpVelocity;
+
+
+    rb.velocity = new Vector2(hortizontal, vertical);
+  }
+}
